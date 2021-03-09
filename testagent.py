42e73a6c6d576e578bca363ee908463fe4a98a1d
Grégoire Retourné <gregoire.retourne@gmail.com>
2021-03-09 17:06:09 +0100

    creation of weight decay + better tb saving

--- testagent.py
+++ testagent.py
@@ -1,6 +1,7 @@
 import gym
 import gym_bilboquet
 import pygame
+import os
 
 from stable_baselines3.sac.policies import MlpPolicy
 from stable_baselines3 import PPO
@@ -35,8 +36,12 @@ def draw_reward():
     implot = plt.imshow(image, cmap='hot')
     plt.show()
 
+def lr(lr_0 = 0.001, decay = 3) :
+    def f(remaining) : # remaining is the proportion of epochs to do (it starts at 1 and finishes at 0 when training ends) 
+        return lr_0 * 1/(1+decay*(1-remaining))
+    return f
 
-def trained_agent(episodes=100,  continuous=True, load=None, save_name="test", ent_coef=0.01):
+def trained_agent(episodes=100,  continuous=True, load=None, save_name="test", ent_coef=0.01, total_timesteps=10000, learning_rate=0.0005):
     env = gym.make("bilboquet-v0", continuous=continuous, amplitude=10)
     env.reset((300, 300))
 
@@ -46,12 +51,13 @@ def trained_agent(episodes=100,  continuous=True, load=None, save_name="test", e
         #         learning_rate=0.001, n_steps=500)
 
         # continuous
-        model = PPO('MlpPolicy', env, verbose=1, batch_size=50,
-                    learning_rate=0.0005, n_steps=500, ent_coef=ent_coef, tensorboard_log="./ppo_bilboquet_tensorboard/")
-        model.learn(total_timesteps=10000)
-        model.save(save_name)
+        model = PPO('MlpPolicy', env, verbose=1, batch_size=128,
+                    learning_rate=learning_rate, n_steps=512, ent_coef=ent_coef, tensorboard_log=f"./ppo_bilboquet_tensorboard/",)
+        model.learn(total_timesteps=total_timesteps, tb_log_name=save_name)
+        model.save(save_name + '.zip')
         print('DONE')
         obs = env.reset()
+
 
     else:
         model = PPO.load(load)
@@ -68,5 +74,5 @@ def trained_agent(episodes=100,  continuous=True, load=None, save_name="test", e
 
 draw_reward()
 if __name__ == "__main__":
-    trained_agent(episodes=500, continuous=True,
-                  load='test0.01.zip', ent_coef=0.01)
+    trained_agent(episodes=1000, continuous=True,
+                  load='test_reward_4_speed_both4', ent_coef=0.001, total_timesteps = 50000, learning_rate=lr())
